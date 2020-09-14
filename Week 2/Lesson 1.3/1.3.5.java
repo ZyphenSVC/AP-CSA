6265ab64d17ba82ffa5af6c0ba7aa160399e0857
ZyphenSVC <undefeatable234@gmail.com>
2020-09-14 16:50:33 -0400

    CodeHS Lesson 1.3

---
+++ Week 2/Lesson 1.3/1.3.5.java
@@ -0,0 +1,26 @@
+public class GoldMedalKarel extends Karel
+{
+    
+    public void run()
+    {
+        move();
+        putBall();
+        move();
+        putBall();
+        move();
+        putBall();
+        turnLeft();
+        move();
+        turnLeft();
+        move();
+        putBall();
+        turnLeft();
+        turnLeft();
+        turnLeft();
+        move();
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }   
+
+}
---
+++ Week 2/Lesson 1.3/Quiz.md
@@ -0,0 +1,14 @@
+### Question 1
+#### What is the run method in a Java program with Karel?
+
+> The method that is called to start a Karel program.
+
+### Question 2
+#### What does a Karel run method look like?
+
+> ```
+> public void run()
+> {
+>     //code 
+> }
+> ```
